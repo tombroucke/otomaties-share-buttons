2f1d328921d5ad427836c9d66135dc9e0e33330b
Tom Broucke <tom@tombroucke.be>
2024-11-25 10:20:02 +0100

    🩹 fix: jit text domain 6.7

--- otomaties-share-buttons.php
+++ otomaties-share-buttons.php
@@ -40,7 +40,7 @@ function init()
     if (! function_exists('get_plugin_data')) {
         require_once(ABSPATH . 'wp-admin/includes/plugin.php');
     }
-    $pluginData = \get_plugin_data(__FILE__);
+    $pluginData = \get_plugin_data(__FILE__, false, false);
     $pluginData['pluginName'] = basename(__FILE__, '.php');
 
     $plugin = new Plugin($pluginData);
